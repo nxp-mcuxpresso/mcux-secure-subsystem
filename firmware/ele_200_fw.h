2688994f123e1f1e16bfd6ab8117e02c98800f30
Patrik Nemeth (nxf98608) <patrik.nemeth@nxp.com>
2026-01-14 15:30:02 +0100

    [MCUX-84608][MCUX-84352] Better loadable FW header handling
    This also fixes a MISRA finding related to a missing declaration
    for the global firmware variable.
    
    Signed-off-by: Patrik Nemeth

--- firmware/ele_200_fw.h
+++ firmware/ele_200_fw.h
@@ -16,11 +16,19 @@
 #ifndef __ELE_FW_H__
 #define __ELE_FW_H__
 
+#if defined(ELEMU_HAS_LOADABLE_FW) && ELEMU_HAS_LOADABLE_FW
+
+extern const uint8_t fw[];
+
 /* We select the correct firmware based on build configuration */
-#if defined(CONFIG_FIRMWARE_S200_EL2GO_KW45) && (CONFIG_FIRMWARE_S200_EL2GO_KW45 > 0)
+#if defined(CONFIG_FIRMWARE_S200_EL2GO_KW45) && CONFIG_FIRMWARE_S200_EL2GO_KW45
 #include "KW45_K32W1xx_MCXW71_SDKFW2.1_RFP.h"
-#elif defined(CONFIG_FIRMWARE_S200_EL2GO_KW47) && (CONFIG_FIRMWARE_S200_EL2GO_KW47 > 0)
+#elif defined(CONFIG_FIRMWARE_S200_EL2GO_KW47) && CONFIG_FIRMWARE_S200_EL2GO_KW47
 #include "KW47_A2_1_SDKFW2_0.h"
+#else
+#error "Compile-time S200 firmware enabled, but no valid configuration selected"
 #endif
+
+#endif /* ELEMU_HAS_LOADABLE_FW */
 
 #endif /* __ELE_FW_H__ */
