850045c2df13a34d0da64273c6ca70b48dc707d1
Patrik Nemeth (nxf98608) <patrik.nemeth@nxp.com>
2026-04-15 17:10:07 +0200

    [MCUX-87087][S200] Update FW to KW47_A2_1_SDKFW4_0
    Signed-off-by: Patrik Nemeth

--- firmware/ele_200_fw.h
+++ firmware/ele_200_fw.h
@@ -24,7 +24,7 @@ extern const uint8_t fw[];
 #if defined(CONFIG_FIRMWARE_S200_EL2GO_KW45) && CONFIG_FIRMWARE_S200_EL2GO_KW45
 #include "KW45_K32W1xx_MCXW71_SDKFW3.0_RFP1.h"
 #elif defined(CONFIG_FIRMWARE_S200_EL2GO_KW47) && CONFIG_FIRMWARE_S200_EL2GO_KW47
-#include "KW47_A2_1_SDKFW3_0.h"
+#include "KW47_A2_1_SDKFW4_0.h"
 #else
 #error "Compile-time S200 firmware enabled, but no valid configuration selected"
 #endif
